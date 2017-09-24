72386c2992c106b39e301fceb4a966216b92097c
Jake <jake@Jakes-MBP.attlocal.net>
2017-09-24 14:09:37 -0500

    working test!

--- app/filterer.js
+++ app/filterer.js
@@ -1,5 +1,30 @@
 
+_ = require('lodash');
+const isTweet = function(tweet) {
+  return _.isString(tweet.id_str) && _.isString(tweet.text) &&
+  tweet.user && _.isString(tweet.user.screen_name)
+}
 
-function filter(tweet) {
-	
+const util = require('util');
+
+exports.filter = function(tweet) {
+  if (! (isTweet(tweet))) { return; }
+  if (! (tweet.entities && tweet.entities.symbols && tweet.entities.symbols.length > 0)) { return; }
+  var retweeted = tweet.retweeted_status
+  if (retweeted) { return; }
+  var symbols = tweet.entities.symbols.map(function(x) { return x.text; });
+  var user = tweet.user.screen_name;
+  var text = tweet.text;
+  
+  filteredInfo = { symbols: symbols, user: user, text: text}
+  return filteredInfo;
+
+  //console.log(message);
+  //console.log(util.inspect(tweet, false, null))
+
+  //slimbot.sendMessage(process.env['TWITTER_KEYWORD_BOT_TELEGRAM_CHAT_ID'], message);
+}
+
+function formatFilteredMessage(obj) {
+  return obj.symbols + ": " + "@" + obj.user + ": " + obj.text + "\n--------------------"
 }
--- test/filterer.spec.js
+++ test/filterer.spec.js
@@ -1,2 +1,21 @@
 var expect    = require("chai").expect;
 var filterer = require("../app/filterer");
+
+describe("Twitter Filterer", function() {
+  it("has message for tweet with symbols", function() {
+    tweet = {
+      id_str: '911960555145109504',
+      text: 'You should buy $EDG',
+      user: {
+        screen_name: '@coinsoncoins'
+      },
+      entities: {
+        symbols: [ { text: 'EDG', indices: [ 15, 19 ] } ]
+      }
+    }
+    obj = filterer.filter(tweet);
+    //expect(JSON.stringify(obj)).to.equal(JSON.stringify({text: 'You should buy $EDG', symbols: ['EDG'], name: '@coinsoncoins'}));
+    expect(obj).to.deep.equal({text: 'You should buy $EDG', symbols: ['EDG'], user: '@coinsoncoins'});
+  })
+
+});
