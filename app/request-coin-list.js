0c53dbed2473de967fc3b36d14dc546461a478e6
Jake <jake@Jakes-MBP.attlocal.net>
2017-09-26 11:23:40 -0500

    add script to get list of official twitter accounts of coins

--- app/request-coin-list.js
+++ app/request-coin-list.js
@@ -6,7 +6,7 @@ axios.get('https://api.coinmarketcap.com/v1/ticker/')
   .then(response => {
     coinDB = response.data;
     coins = coinDB.map(function(x) { return x.symbol; });
-    fs.writeFileSync(__dirname + '/whitelisted-symbols.list', coins.join("\n"));
+    fs.writeFileSync(__dirname + '/data/whitelisted-symbols.list', coins.join("\n"));
   })
   .catch(error => {
     console.log(error);
---
+++ app/request-coin-twitter-usernames.js
@@ -0,0 +1,36 @@
+
+
+var TwitterPackage = require('twitter');
+var fs = require('fs');
+
+var secret = {
+  consumer_key: process.env.TWITTER_KEYWORD_BOT_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_KEYWORD_BOT_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_KEYWORD_BOT_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_KEYWORD_BOT_ACCESS_TOKEN_SECRET
+}
+var client = new TwitterPackage(secret);
+
+
+promises = [];
+promises[0] = client.get('lists/members', {slug: 'coins', owner_screen_name: 'coinsoncoins', count: 5000})
+promises[1] = client.get('lists/members', {slug: 'coins-5-50', owner_screen_name: 'coinsoncoins'})
+promises[2] = client.get('lists/members', {slug: 'coins-50-200', owner_screen_name: 'coinsoncoins'})
+
+Promise.all(promises)
+.then(values => {
+  set = new Set()
+  for(var i=0; i<values.length; i++) {
+    names = values[i].users.map(function(u) { return u.screen_name; });
+
+    names.forEach(function(e) {
+      set.add(e);
+    });
+  }
+  screen_names = Array.from(set);
+  fs.writeFileSync(__dirname + '/data/official-accounts.list', screen_names.join("\n"));
+})
+.catch(err => {
+    console.log(reason);
+});
+
