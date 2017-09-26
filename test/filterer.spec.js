32951f1f39f0e5d65895ab7d9d7e972140b947e7
Jake <jake@Jakes-MBP.attlocal.net>
2017-09-26 08:10:21 -0500

    clean up tests

--- test/filterer.spec.js
+++ test/filterer.spec.js
@@ -9,18 +9,23 @@ textToIgnore = ["insider selling", "public offering"];
 
 const filterer = new Filterer(keywords, whitelistedSymbols, symbolsToIgnore, usersToIgnore, textToIgnore);
 
+createTweet = function() {
+  tweet = { // this tweet should return an object
+    id_str: '911960555145109504',
+    text: 'You should buy $EDG',
+    user: {
+      screen_name: 'coinsoncoins'
+    },
+    entities: {
+      symbols: [ { text: 'EDG', indices: [ 15, 19 ] } ]
+    }
+  }
+  return tweet;
+}
+
 describe("Twitter Filterer", function() {
   it("has message for tweet with symbols in whitelist", function() {
-    tweet = {
-      id_str: '911960555145109504',
-      text: 'You should buy $EDG',
-      user: {
-        screen_name: 'coinsoncoins'
-      },
-      entities: {
-        symbols: [ { text: 'EDG', indices: [ 15, 19 ] } ]
-      }
-    }
+    tweet = createTweet();
     obj = filterer.filter(tweet);
     expect(obj).to.deep.equal({text: 'You should buy $EDG', symbols: ['EDG'], user: 'coinsoncoins', keyword: "buy"});
     tweet.entities.symbols[0].text = tweet.entities.symbols[0].text.toLowerCase();
@@ -28,40 +33,15 @@ describe("Twitter Filterer", function() {
   });
 
   it("returns nothing for an invalid tweet", function() {
-    tweet = { // no user
-      id_str: '911960555145109504',
-      text: 'You should buy $EDG'
-    }
+    tweet = createTweet();
+    delete tweet.user;
     expect(filterer.filter(tweet)).to.not.be.ok;
   });
 
   it("returns nothing for a symbol not in the whitelist", function() {
-    tweet = {
-      id_str: '911960555145109504',
-      text: 'You should buy $EDG',
-      user: {
-        screen_name: 'coinsoncoins'
-      },
-      entities: {
-        symbols: [ { text: 'USDJPY', indices: [ 15, 19 ] } ]
-      }
-    }
+    tweet = createTweet();
+    tweet.entities.symbols[0].text = 'USDJPY';
     expect(filterer.filter(tweet)).to.not.be.ok;
-  });
-
-  it("passes back the right keyword", function() {
-    tweet = {
-      id_str: '911960555145109504',
-      text: 'You should burn $EDG',
-      user: {
-        screen_name: 'coinsoncoins'
-      },
-      entities: {
-        symbols: [ { text: 'EDG', indices: [ 15, 19 ] } ]
-      }
-    }
-    obj = filterer.filter(tweet);
-    expect(obj.keyword).to.equal("burn");
   });
 
   it("passes back the right keyword if it's in the quoted_status", function() {
@@ -78,40 +58,26 @@ describe("Twitter Filterer", function() {
         symbols: [ { text: 'EDG', indices: [ 15, 19 ] } ]
       }
     }
+    tweet = createTweet();
+    tweet.text = "hey";
+    tweet.quoted_status = { text: "You should buy $EDG" };
     obj = filterer.filter(tweet);
-    expect(obj).to.deep.equal({text: 'Hey look at this $EDG', symbols: ['EDG'], user: 'coinsoncoins', 
+    expect(obj).to.deep.equal({text: 'hey', symbols: ['EDG'], user: 'coinsoncoins', 
       keyword: "", quotedText: "You should buy $EDG", quotedKeyword: "buy"});
   });
 
 
   // it("returns nothing for symbol in the symbolsToIgnore list", function() {
-  //   tweet = {
-  //     id_str: '911960555145109504',
-  //     text: '$USD is strong',
-  //     user: {
-  //       screen_name: 'coinsoncoins'
-  //     },
-  //     entities: {
-  //       symbols: [ { text: 'BTC', indices: [ 0, 4 ]}, { text: 'USD', indices: [ 0, 4 ] } ]
-  //     }
-  //   }
-  //   obj = filterer.filter(tweet);
-  //   expect(obj).to.not.be.ok;
+  //   tweet = createTweet();
+  //   tweet.entities.symbols.push({ text: 'USD', indices: [ 0, 4 ] });
+  //   expect(filterer.filter(tweet)).to.not.be.ok;
   // });
 
+
   it("returns nothing for user in the usersToIgnore list", function() {
-    tweet = {
-      id_str: '911960555145109504',
-      text: 'You should buy $EDG',
-      user: {
-        screen_name: 'stocknewsBOT'
-      },
-      entities: {
-        symbols: [ { text: 'EDG', indices: [ 15, 19 ] } ]
-      }
-    }
-    obj = filterer.filter(tweet);
-    expect(obj).to.not.be.ok;
+    tweet = createTweet();
+    tweet.user.screen_name = 'stocknewsBOT'
+    expect(filterer.filter(tweet)).to.not.be.ok;
   });
 
   // it("returns nothing for text in the textToIgnore list", function() {
